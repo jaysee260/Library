6040386acfb63527b8e5800d052a64bf6d8e94f8
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-12-29 19:19:18 -0500

    add Library.FrontDesk (auth service). still in POC stage.

---
+++ Library.FrontDesk/Configuration/Authentication.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class Authentication
+    {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                
+                    ValidIssuer = configuration.GetSection("JwtToken").GetValue<string>("Issuer"),
+                    ValidAudience = configuration.GetSection("JwtToken").GetValue<string>("Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(
+                            configuration.GetSection("JwtToken").GetValue<string>("Base64Secret")
+                        )
+                    )
+                };
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Library.FrontDesk/Controllers/AuthController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Library.FrontDesk.Controllers
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Permissions
+    {
+        public bool Write { get; set; }
+        public bool Read { get; set; }
+        public bool Update { get; set; }
+        public bool Delete { get; set; }
+
+        public Permissions(bool allowAl)
+        {
+            if (allowAl)
+            {
+                Write = true;
+                Read = true;
+                Update = true;
+                Delete = true;
+            }
+        }
+    }
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid request; please provide username and password");
+            }
+
+            if (user.Username == "jaysee" && user.Password == "pass321")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    _configuration.GetSection("JwtToken").GetValue<string>("Base64Secret")
+                ));
+                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: _configuration.GetSection("JwtToken").GetValue<string>("Issuer"),
+                    audience: _configuration.GetSection("JwtToken").GetValue<string>("Audience"),
+                    claims: new List<Claim>
+                    {
+                        new Claim("userId", "1"),
+                        new Claim("role", "admin")
+                        // new Claim("permissions", JsonSerializer.Serialize(new Permissions(true)))
+                    },
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signingCredentials
+                );
+            
+                var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpGet("validate")]
+        [Authorize]
+        public IActionResult ValidateIdentity()
+        {
+            return Ok("you're good to go");
+        }
+    }
+}
