6c1ca98057f1531ea62c7d232c18bf05a7164050
Juan Carlos Gonzalez <programmer1718@gmail.com>
2020-12-30 19:15:43 -0500

    beginning of integration tests

---
+++ Library.Api.IntegrationTests/Books/AddBookTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Library.Contracts.RestApi;
+using Library.Testing.Data;
+using Library.Testing.Extensions;
+using Library.Testing.Utilities;
+using Xunit;
+
+namespace Library.Api.IntegrationTests.Books
+{
+    public class AddBookTests : IClassFixture<LibraryApiFixture>
+    {
+        private readonly LibraryApiFixture _fixture;
+        private readonly HttpClientWrapper _client;
+        private const string Url = "/books";
+
+        public AddBookTests(LibraryApiFixture fixture)
+        {
+            _fixture = fixture;
+            _client = new HttpClientWrapper(fixture.CreateClient());
+        }
+
+        [Fact]
+        public async Task AddBook_ReturnsOkResponse_WithCreatedBook()
+        {
+            var bookDto = TestData.BookDto;
+            var response = await _client.PostAsync(Url, bookDto);
+            var content = response.GetResponseContent<BookDto>();
+            
+            response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.OK);
+            content.Should().NotBeNull();
+            content.Id.Should().NotBeEmpty();
+        }
+    }
+}
